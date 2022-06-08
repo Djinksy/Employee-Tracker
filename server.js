869256db4a93ba58af39bccd551dce731096f582
Djinksy <damienjinks39@gmail.com>
2022-06-08 14:37:38 +1000

    update test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'root',
+        password: process.env.DB_PASSWORD,
         database: 'employee_db'
     },
     console.log(`Connected to the employee_db database`)
